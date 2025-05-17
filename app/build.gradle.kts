b7d37fda242df89fb224585a8883f9f30a3563b1
Joseph Camilo <joseph_camilo@ucne.edu.do>
2025-05-17 18:59:30 -0400

    Agregando la navegacion al proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.0")
 
     //navegacion
-    implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlin.serialization.json)
 
     //room
--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -4,44 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
-import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnicos.presentation.navigation.RegistroTecnicosNavHost
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoDb: TecnicoDb
@@ -59,161 +26,9 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             RegistroTecnicosTheme {
-
-
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-                        TecnicoScreen()
-                    }
-                }
+                val navHot = rememberNavController()
+                RegistroTecnicosNavHost(navHot)
             }
-        }
-    }
-
-    @Composable
-    fun TecnicoScreen(
-    ) {
-        var nombre by remember { mutableStateOf("") }
-        var sueldo by remember { mutableStateOf("") }
-        var errorMessage: String? by remember { mutableStateOf(null) }
-
-        Scaffold { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-                    .padding(8.dp)
-            ) {
-                ElevatedCard(
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                    ) {
-
-                        OutlinedTextField(
-                            label = { Text(text = "Nombre") },
-                            value = nombre,
-                            onValueChange = { nombre = it },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        OutlinedTextField(
-                            label = { Text(text = "Sueldo") },
-                            value = sueldo,
-                            onValueChange = { sueldo = it },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.padding(2.dp))
-                        errorMessage?.let {
-                            Text(text = it, color = Color.Red)
-                        }
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                        ) {
-                            OutlinedButton(
-                                onClick = {
-
-                                }
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Add,
-                                    contentDescription = "save button"
-                                )
-                                Text(text = "Nuevo")
-                            }
-                            val scope = rememberCoroutineScope()
-                            OutlinedButton(
-                                onClick = {
-                                    if (nombre.isBlank())
-                                        errorMessage = "Nombre vacio"
-
-                                    scope.launch {
-                                        saveTecnico(
-                                            TecnicoEntity(
-                                                nombre = nombre,
-                                                sueldo = sueldo
-                                            )
-                                        )
-                                        nombre = ""
-                                        sueldo = ""
-                                    }
-                                }
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Edit,
-                                    contentDescription = "save button"
-                                )
-                                Text(text = "Guardar")
-                            }
-                        }
-                    }
-                }
-
-                val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
-                val tecnicoList by tecnicoDb.tecnicoDao().getAll()
-                    .collectAsStateWithLifecycle(
-                        initialValue = emptyList(),
-                        lifecycleOwner = lifecycleOwner,
-                        minActiveState = Lifecycle.State.STARTED
-                    )
-                TecnicoListScreen(tecnicoList)
-            }
-        }
-    }
-
-    @Composable
-    fun TecnicoListScreen(tecnicoList: List<TecnicoEntity>) {
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Spacer(modifier = Modifier.height(32.dp))
-            Text("Lista de tecnicos")
-
-            LazyColumn(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                items(tecnicoList) {
-                    TecnicoRow(it)
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun TecnicoRow(it: TecnicoEntity) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(modifier = Modifier.weight(1f), text = it.tecnicoId.toString())
-            Text(
-                modifier = Modifier.weight(2f),
-                text = it.nombre,
-                style = MaterialTheme.typography.headlineLarge
-            )
-            Text(modifier = Modifier.weight(2f), text = it.sueldo)
-        }
-        HorizontalDivider()
-    }
-
-    private suspend fun saveTecnico(tecnico: TecnicoEntity) {
-        tecnicoDb.tecnicoDao().save(tecnico)
-    }
-
-    @Preview(showBackground = true, showSystemUi = true)
-    @Composable
-    fun Preview() {
-        RegistroTecnicosTheme {
-            val tecnicoList = listOf(
-                TecnicoEntity(1, "Joseph Camilo", "10,000"),
-                TecnicoEntity(2, "Victor Estevez", "20,000"),
-            )
-            TecnicoListScreen(tecnicoList)
         }
     }
 }
